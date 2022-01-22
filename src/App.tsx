3afdb491caec1f98160ef4c0e5c9e20fe85958ef
AndroidLollipop <obfuscated00143241131643657478@gmail.com>
2022-01-22 17:25:44 +0800

    almost done

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,20 @@ const App = () => {
                 <div className="ImageContainer col-3 col-sm-2 col-md-12 col-lg-12">
                   <img className="Image" src="https://www.nodeflair.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZ21UIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--4a34bcb984abb4039442899477a50dbbc0011076/Binance.png" alt="binance"/>
                 </div>
+                <div className="DescriptionContainer col-9 col-sm-10 col-md-12 col-lg-12">
+                  <p className="CompanyName">Binance</p>
+                  <div className="JobTitle">DevOps Engineer (C2C)</div>
+                </div>
               </div>
               <div className="Top2">
-                Top2
+                <div className="col-6 centered">
+                  <div className="Elapsed">
+                    1 minute ago
+                  </div>
+                </div>
+                <div className="col-6">
+                  <span className="Field">DevOps</span>
+                </div>
               </div>
             </div>
             <div className="Bottom">Bottom</div>
